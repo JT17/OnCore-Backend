6d252c59dd08ae07cb4d1f13dd7e974212f06231
Alex Markovits <akmarkov@mit.edu>
2016-05-05 18:20:21 -0400

    merge

--- application.py
+++ application.py
@@ -221,7 +221,7 @@ def index():
     return render_template('index.html', appts=[], patients=[])
 
 if __name__ == '__main__':
-    import os
-    os.remove('application/test.db')
+    # import os
+    # os.remove('application/test.db')
     db.create_all()
     application.run(host='0.0.0.0')
