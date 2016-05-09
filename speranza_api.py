68255ae3198229743352ecc35716734fb06d5706
Jonathan <tiaojon@umich.edu>
2016-05-08 22:44:46 -0400

    less output lol

--- speranza_api.py
+++ speranza_api.py
@@ -95,9 +95,6 @@ def checkin_out(request, checkin = True):
 #returns False if invalid request, True otherwise
 def verify_new_user(request):
 	print request.form
-	print request.form['firstname']
-	print request.form['lastname']
-	print request.form['phone_number']
 	if 'firstname' not in request.form or 'lastname' not in request.form or 'phone_number' not in request.form \
 		or 'contact_number' not in request.form: 
 		return False;
