3467d0e7e03584a2287211b850c7ef5c26fab0e0
Alex Markovits <akmarkov@mit.edu>
2016-06-12 13:40:28 -0700

    push to rds db

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://speranzaadmin:tunesquad3@speranzabetajune.cmzfo5lzx48b.us-east-1.rds.amazonaws.com:3306/betajunedb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://speranzaadmin:tunesquad3@speranzabeta.cmzfo5lzx48b.us-east-1.rds.amazonaws.com:3306/betajunedb'
 
 # Uncomment the line below if you want to work with a local DB
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 
-#SQLALCHEMY_POOL_SIZE = 20
-#SQLALCHEMY_POOL_RECYCLE = 280
+SQLALCHEMY_POOL_SIZE = 20
+SQLALCHEMY_POOL_RECYCLE = 280
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
