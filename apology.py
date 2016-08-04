e1b9cd6209294f6f1cafd1489304d74d865e00cc
Jonathan <tiaojon@umich.edu>
2016-08-03 23:47:03 -0700

    skeleton script for apology texts

---
+++ apology.py
@@ -0,0 +1,26 @@
+from application.models import *
+from flask import Flask, request, redirect, jsonify, g
+import datetime
+import requests
+from messenger import send_message
+
+# FRONTLINESMS_API_KEY = "309fefe6-e619-4766-a4a2-53f0891fde23"
+# FRONTLINESMS_WEBHOOK = "https://cloud.frontlinesms.com/api/1/webhook"
+# -*- coding: utf-8 -*-
+
+def send_diabetes_reminders():
+	try:
+		numbers = [FILL IN HERE]	
+		for num in numbers:
+			message = "Hola. Los sentimos para enviarse los mensajes anteayer. Fue un error, y puede ignorarlos. Los sientos!"
+			send_message(message, patient.phone_number);
+	except ValueError, e:
+		return str(e);
+
+def is_number(s):
+	try:
+		float(s)
+		return True;
+	except ValueError:
+		return False
+
